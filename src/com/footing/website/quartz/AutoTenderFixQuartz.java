f67310595ca40e7620cb80146fbb9123c7b9008b
yubin1030 <yubinpll9110@163.com>
2016-06-01 14:40:12 +0800

    第一次提交

---
+++ src/com/footing/website/quartz/AutoTenderFixQuartz.java
@@ -0,0 +1,25 @@
+package com.footing.website.quartz;
+
+import org.apache.log4j.Logger;
+
+import com.footing.website.common.utils.DateUtils;
+
+/**
+ * 
+ * <p>
+ * Description:定时任务测试推送<br />
+ * </p>
+ * @title AutoTenderFixQuartz.java
+ * @package com.footing.website.quartz 
+ * @author yubin
+ * @version 0.1 2016年3月4日
+ */
+public class AutoTenderFixQuartz {
+    
+	private  static final Logger logger=Logger.getLogger(AutoTenderFixQuartz.class);
+    
+	public void doAutoTenderFix(){
+		logger.info(DateUtils.getDateTime());
+		logger.info("定时任务测试推送"+AutoTenderFixQuartz.class.toString());
+	}
+}
